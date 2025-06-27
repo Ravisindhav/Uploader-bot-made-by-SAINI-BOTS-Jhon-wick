d1b62c65ff3caec6e41a501b2cb882b3d9934023
Ravisindhav <sindhavravi11@gmail.com>
2025-06-27 13:51:31 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from telegram.ext import Updater, CommandHandler, MessageHandler
-client = MongoClient("mongodb+srv://sindhavravi11:$Sind1234567890@cluster0.bwlgbx3.mongodb.net/")
+client = MongoClient("mongodb+srv://sindhavravi11:%24Sind1234567890@cluster0.bwlgbx3.mongodb.net/")
 TOKEN = 'aapka_telegram_bot_token'
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='Bot chal gaya hai!')
